e2939f5af2fe53f85fa81c1ee617f2a84c0cefaa
seungjunseo <newworld1423@naver.com>
2024-10-31 11:07:28 +0900

    테스트

--- js/script.js
+++ js/script.js
@@ -1,22 +1,13 @@
-// 체크박스 상태를 로컬 스토리지에 저장하는 함수
-function saveCheckboxState() {
+// 체크박스 상태를 URL 파라미터로 전송하는 함수
+function generateCheckedUrl() {
+    const paramInput = document.getElementById("params").value; // 파라미터 입력값 가져오기
     const checkedIds = Array.from(document.querySelectorAll("input[type='checkbox']"))
         .filter(checkbox => checkbox.checked)
         .map(checkbox => checkbox.id);
 
-    localStorage.setItem("checkedCheckboxes", JSON.stringify(checkedIds));
-}
-
-// 체크박스 체크 시 이벤트 리스너 추가
-document.querySelectorAll("input[type='checkbox']").forEach(checkbox => {
-    checkbox.addEventListener("change", saveCheckboxState);
-});
-
-// 체크된 항목 URL로 이동하는 함수
-function goToCheckedUrl() {
-    const checkedIds = JSON.parse(localStorage.getItem("checkedCheckboxes") || "[]");
     const params = new URLSearchParams();
-    params.set("ssj", checkedIds.join(","));
+    params.set("param", paramInput); // 입력된 파라미터 값 추가
+    params.set("checked", checkedIds.join(",")); // 체크된 체크박스 ID 추가
 
     // URL 이동
     window.location.href = `https://html-is-a-programming-language.github.io/pocket/?${params.toString()}`;
@@ -25,7 +16,7 @@ function goToCheckedUrl() {
 // URL에서 체크된 항목을 불러오는 함수
 function loadCheckboxStateFromUrl() {
     const urlParams = new URLSearchParams(window.location.search);
-    const checkedIds = urlParams.get("ssj");
+    const checkedIds = urlParams.get("checked");
 
     if (checkedIds) {
         checkedIds.split(",").forEach(id => {
